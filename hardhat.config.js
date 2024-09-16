6b3ae1fcdf3cc4b05757667a5cac0eb11a951b00
Denis Glotov <denis@glotov.org>
2024-09-16 13:34:06 +0300

    add Github CI

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
